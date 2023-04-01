b92ddffff73f1bbf3f7adaf88b6b006b171520c5
kacperpucylo <pucylek@wp.pl>
2023-04-01 21:50:18 +0200

    i'm not sure i understand the whole shader thing, and at this point i'm too afraid to ask

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 extern crate sdl2;
 extern crate gl;
-
 // open gl docs locally -> cargo doc -p gl --no-deps --open
-//
+
+pub mod render_gl;
 
 fn main()
 {
     // println!("Hello, world!");
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+	let gl_attr = video_subsystem.gl_attr();
+
+	// set opengl to core profile
+	gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+	// set version to 4.5
+	gl_attr.set_context_version(4, 5);
+
     let window = video_subsystem.window("Scop", 900, 700).opengl().resizable().build().unwrap();
 
     let _gl_context = window.gl_create_context().unwrap();
@@ -16,9 +23,28 @@ fn main()
 
     let mut event_pump = sdl.event_pump().unwrap();
 
-    unsafe {
+    unsafe
+	{
+		gl::Viewport(0, 0, 900, 700);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
+
+	// leave the use here, bc ideally the following code will be moved to another file
+	use std::ffi::CString;
+
+	let vert_shader = render_gl::Shader::from_vert_source(
+		&CString::new(include_str!("triangle.vert")).unwrap()
+	).unwrap();
+
+	let frag_shader = render_gl::Shader::from_frag_source(
+		&CString::new(include_str!("triangle.frag")).unwrap()
+	).unwrap();
+
+	let shader_program = render_gl::Program::from_shaders(
+		&[vert_shader, frag_shader]
+	).unwrap();
+
+	shader_program.set_used();
 
     'main: loop
     {
@@ -40,3 +66,4 @@ fn main()
         // render contents
     }
 }
+
