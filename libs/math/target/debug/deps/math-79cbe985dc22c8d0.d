e948c5eefe41edf3ea8d55713727cd18ce3d6af3
kacperpucylo <pucylek@wp.pl>
2023-04-07 03:24:18 +0200

    basic vector math

--- libs/math/target/debug/deps/math-79cbe985dc22c8d0.d
+++
@@ -1,8 +0,0 @@
-D:\dev\scop\libs\math\target\debug\deps\math-79cbe985dc22c8d0.exe: src\lib.rs src\vector\mod.rs src\vector\vector2.rs src\vector\vector3.rs
-
-D:\dev\scop\libs\math\target\debug\deps\math-79cbe985dc22c8d0.d: src\lib.rs src\vector\mod.rs src\vector\vector2.rs src\vector\vector3.rs
-
-src\lib.rs:
-src\vector\mod.rs:
-src\vector\vector2.rs:
-src\vector\vector3.rs:
--- libs/math/target/debug/libmath.d
+++
@@ -1,1 +0,0 @@
-D:\dev\scop\libs\math\target\debug\libmath.rlib: D:\dev\scop\libs\math\src\lib.rs
